aeecdfe6050000c17991b189129695da7c9a0fba
aleksandraobucina <153324527+aleksandraobucina@users.noreply.github.com>
2023-12-11 17:54:59 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,12 +125,14 @@ const poruciForm = document.getElementById('poruciForm');
 poruciForm.addEventListener('submit', function(event) {
   event.preventDefault();
   const email = document.getElementById('email').value;
+  const ime = document.getElementById('ime').value;
+  const prezime = document.getElementById('prezime').value;
 
-  if (validateEmail(email)) {
+  if (validateEmail(email) && validateName(ime) && validateName(prezime)) {
     alert('Porudžba uspešna!');
     modal.style.display = 'none';
   } else {
-    alert('Unesite validan email!');
+    alert('Proverite podatke!');
   }
 });
 
@@ -138,6 +140,12 @@ function validateEmail(email) {
   const re = /\S+@\S+\.\S+/;
   return re.test(String(email).toLowerCase());
 }
+
+function validateName(name) {
+  const re = /^[a-zA-Z]{2,}$/;
+  return re.test(name);
+}
+
 
 const noviTelefoniData = [
   { ime: 'Telefon 16', slika: 'assets/img/ultra.png', cena: '$799', sifra: 'ULT01' },
@@ -236,22 +244,46 @@ function generateContactForm() {
 
   contactForm.appendChild(form);
 
-  // Validacija forme pre slanja
-  form.addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    if (form.checkValidity()) {
-      const formData = new FormData(form);
-      const formDataObject = {};
-      formData.forEach((value, key) => {
-        formDataObject[key] = value;
-      });
-      console.log(formDataObject);
-      form.reset();
-    } else {
-      alert('Molimo vas da ispravno popunite sva polja forme.');
-    }
-  });
-}
+   // Postavljanje regularnih izraza za ime i prezime
+   nameInput.setAttribute('pattern', '[a-zA-ZčćžšđČĆŽŠĐ\\s]{2,30}');
+   nameInput.setAttribute('title', 'Unesite vaše ime (2-30 karaktera, samo slova)');
+ 
+   lastNameInput.setAttribute('pattern', '[a-zA-ZčćžšđČĆŽŠĐ\\s]{2,30}');
+   lastNameInput.setAttribute('title', 'Unesite vaše prezime (2-30 karaktera, samo slova)');
+ 
+  
+   form.addEventListener('submit', function(event) {
+
+     event.preventDefault();
+ 
+     // Validacija unosa
+     const nameValue = nameInput.value.trim();
+     const lastNameValue = lastNameInput.value.trim();
+     const emailValue = emailInput.value.trim();
+ 
+     const nameRegex = /[a-zA-ZčćžšđČĆŽŠĐ]{2,}/;
+     const lastNameRegex = /[a-zA-ZčćžšđČĆŽŠĐ]{2,}/;
+     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+ 
+     if (!nameRegex.test(nameValue)) {
+       alert("Ime nije ispravno uneto. Ime mora imati najmanje dva slova.");
+       return;
+     }
+ 
+     if (!lastNameRegex.test(lastNameValue)) {
+       alert("Prezime nije ispravno uneto. Prezime mora imati najmanje dva slova.");
+       return;
+     }
+ 
+     if (!emailRegex.test(emailValue)) {
+       alert("E-mail adresa nije ispravno uneta.");
+       return;
+     }
+
+     alert("Uspesno ste poslali poruku.");
+   });
+ 
+ }
 
 generateContactForm();
 
